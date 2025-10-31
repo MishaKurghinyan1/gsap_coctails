bed40fd5b4efc24ca28ed3d4ed0fa31d49deeca8
MishaKurghinyan1 <minecon291@gmail.com>
2025-10-31 21:26:17 +0400

    Updated positioning for phones, removing cocktail leaves on phones

--- src/components/Cocktails.tsx
+++ src/components/Cocktails.tsx
@@ -1,42 +1,72 @@
 import { useGSAP } from "@gsap/react";
 import { cocktailLists, mockTailLists } from "../../constants";
 import gsap from "gsap";
+import { useMediaQuery } from "react-responsive";
 
 const Cocktails = () => {
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+
   useGSAP(() => {
-    const parallaxTimeline = gsap.timeline({
-      scrollTrigger: {
-        trigger: "#cocktails",
-        start: "top 30%",
-        end: "bottom 80%",
-        scrub: true,
-      },
-    });
-    parallaxTimeline
-      .from("#c-left-leaf", {
-        x: -100,
-        y: 100,
-        ease: "none",
-      })
-      .from("#c-right-leaf", {
-        x: 100,
-        y: 100,
-        ease: "none",
+    if (!isMobile) {
+      const parallaxTimelineLeft = gsap.timeline({
+        scrollTrigger: {
+          trigger: "#cocktails",
+          start: "top 30%",
+          end: "bottom 80%",
+          scrub: 1,
+        },
       });
+
+      const parallaxTimelineRight = gsap.timeline({
+        scrollTrigger: {
+          trigger: "#cocktails",
+          start: "top 50%",
+          end: "bottom 80%",
+          scrub: 1,
+        },
+      });
+      parallaxTimelineLeft.fromTo(
+        "#c-left-leaf",
+        {
+          x: -150,
+          y: 100,
+        },
+        {
+          x: 0,
+          y: 0,
+        }
+      );
+      parallaxTimelineRight.fromTo(
+        "#c-right-leaf",
+        {
+          x: 100,
+          y: 100,
+          ease: "power1.out",
+        },
+        {
+          x: -10,
+          y: 50,
+          ease: "power1.out",
+        }
+      );
+    }
   }, []);
   return (
     <section id="cocktails" className="noisy">
-      <img
-        src="/images/cocktail-left-leaf.png"
-        alt="left-leaf"
-        id="c-left-leaf"
-      />
-      <img
-        src="/images/cocktail-right-leaf.png"
-        alt="right-leaf"
-        id="c-right-leaf"
-      />
-
+      {!isMobile && (
+        <>
+          <img
+            src="/images/cocktail-left-leaf.png"
+            alt="left-leaf"
+            id="c-left-leaf"
+          />
+          <img
+            src="/images/cocktail-right-leaf.png"
+            alt="right-leaf"
+            id="c-right-leaf"
+          />
+        </>
+      )}
       <div className="list">
         <div className="popular">
           <h2>Most popular Cocktails:</h2>
